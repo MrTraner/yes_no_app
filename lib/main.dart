510159c54edb303bc5f42e3c355912104bab6784
MrTraner <juanmauricio.ochoa@hotmail.com>
2023-08-06 14:22:14 -0600

    AppTheme
    Se agrega nuevo tema

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,15 +11,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 4).theme(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
         body: Center(
           child: FilledButton.tonal(
-            onPressed: () {},
-            child: const Text('Click me')
-          ),
+              onPressed: () {}, child: const Text('Click me')),
         ),
       ),
     );
