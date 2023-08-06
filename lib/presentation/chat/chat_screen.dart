72e64567cefdf088575cd1dd036824601fa9a329
MrTraner <juanmauricio.ochoa@hotmail.com>
2023-08-06 14:49:03 -0600

    Widget _ChatView
    Se agrega widget para la vista del chat usando un ListView

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -6,16 +6,34 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Padding(
-          padding: EdgeInsets.all(4.0),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage('https://1.bp.blogspot.com/-K5xmMaubk58/YAWr34GnO_I/AAAAAAAA0L0/WadlEdUv8hsfsB-zv_mW5kNE6vnjLXFzgCLcBGAsYHQ/s2048/Elizabeth%2BOlsen%2BGawalpop.jpg')
-          ),
-        ),
-        title: const Text('Elizabeth ❤'),
-        centerTitle: true
-      ),
-    );
+        appBar: AppBar(
+            leading: const Padding(
+                padding: EdgeInsets.all(4.0),
+                child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://1.bp.blogspot.com/-K5xmMaubk58/YAWr34GnO_I/AAAAAAAA0L0/WadlEdUv8hsfsB-zv_mW5kNE6vnjLXFzgCLcBGAsYHQ/s2048/Elizabeth%2BOlsen%2BGawalpop.jpg'))),
+            title: const Text('Elizabeth ❤'),
+            centerTitle: true),
+        body: _ChatView());
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(children: [
+          Expanded(
+              child: ListView.builder(
+                  itemCount: 100,
+                  itemBuilder: (context, index) {
+                    return Text('Indice: $index');
+                  })),
+          const Text('Hola'),
+          const Text('Mundo')
+        ])
+    ));
   }
 }
