bdcfec5a331f1738922e667dfbc351723478100d
MrTraner <juanmauricio.ochoa@hotmail.com>
2023-08-06 18:31:10 -0600

    Argumentos a MyMessageBubble
    Se agrega la entidad Message como argumento al widget MyMessageBubble

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -41,7 +41,7 @@ class _ChatView extends StatelessWidget {
                     final message = chatProvider.messageList[index];
                     return (message.fromWho == FromWho.hers)
                       ? const HerMessageBubble()
-                      : const MyMessageBubble();
+                      : MyMessageBubble(message: message);
                   })),
           //Caja de texto de mensajes
           const MessageFieldBox()
